c249c977c20f3e3c689fa1ae7e752d66a2b5755b
Max Andrade <max.andrade@gmail.com>
2025-06-11 12:15:31 +0930

    update

--- Investment Tracker/config.ts
+++ Investment Tracker/config.ts
@@ -10,7 +10,7 @@ export const AZURE_STORAGE_CONFIG = {
 
   // Your SAS token for the container.
   // Example: "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupx&se=2024-12-31T23:59:59Z&st=2023-01-01T00:00:00Z&spr=https&sig=yourActualSignature"
-  sasToken: "sp=racwl&st=2025-06-06T06:17:34Z&se=2099-06-06T14:17:34Z&spr=https&sv=2024-11-04&sr=c&sig=Zf3o58rGnDCu%2BO0SPSnEEW5Mi5jin%2BRtMIGw5%2B7jUJw%3D",
+  sasToken: "sp=racwl&st=2025-06-06T06:17:34Z&se=2099-06-06T14:17:34Z&spr=https&sv=2024-11-04&sr=c&sig=TBD",
   
   // The name of the blob (file) where the application data will be stored.
   fileName: "investment-tracker-config.json"
